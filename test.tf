cdbf99f82c15ef48c8d9e39a0be7ccbd14ef6331
anilo2021 <95632935+anilo2021@users.noreply.github.com>
2022-11-28 20:03:19 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,1 +1,3 @@
 # this is for test purpose
+
+https://teams.microsoft.com/l/meetup-join/19%3ameeting_NTEzYzhkYzgtZmZjNi00ZTgxLTgyNWMtMzZkNmFkMWJjMGIy%40thread.v2/0?context=%7b%22Tid%22%3a%2242f7676c-f455-423c-82f6-dc2d99791af7%22%2c%22Oid%22%3a%22658c7995-eb91-4475-8750-3ab4ed9d67aa%22%7d
