b26f56965f906b7b89a7ec1152a359ee1003a898
anilo2021 <95632935+anilo2021@users.noreply.github.com>
2022-12-01 20:50:20 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,3 +1,29 @@
 # this is for test purpose
 
 https://teams.microsoft.com/l/meetup-join/19%3ameeting_NTEzYzhkYzgtZmZjNi00ZTgxLTgyNWMtMzZkNmFkMWJjMGIy%40thread.v2/0?context=%7b%22Tid%22%3a%2242f7676c-f455-423c-82f6-dc2d99791af7%22%2c%22Oid%22%3a%22658c7995-eb91-4475-8750-3ab4ed9d67aa%22%7d
+
+
+Dear All,
+
+Work from office has been started for everyone hence MMT request are not been consider, requesting you all to Submit Assets in person to TCS Victor Building, shared below and get confirmation email from security that will ease the process for your FFS also. 
+1.	You are required to submit all the assets to Victor Admin team before your  LWD i.e. as per below table. 
+2.	Get mail confirmation after submitting TCS Assets from TCS Victory Security Team 'Victor Security' victor.security@tcs.com and cc these ID’s s.hanvalamath@sap.com ,lakshmi.prasanna.karanam@sap.com, sivaranjani.thanigachalam@sap.com, ravali.pachabotla@sap.com, Odela, anusha.odela@sap.com.
+Address:- To 
+TCS Victor Bangalore Admin, 
+Tata Consultancy Services Limited 
+(Unit V),Ground Floor, 3rd Floor-13th Floor, 
+Victor Building (MTB-3),ITPL SEZ, 
+Whitefield Road, Bangalore - 560066
+Ph:9108038606 
+
+Assets include the following: 
+#	Asset Description	Make	Asset ID
+1	Desktop	 	 
+2	Laptop	 	 
+3	Charger	 	 
+4	Keyboard	 	 
+5	Mouse	 	 
+6	TCS ID Card	 	 
+7	SEZ ID Card	 	 
+8	RSA Card	 	 
+9	Headphones	 	 
